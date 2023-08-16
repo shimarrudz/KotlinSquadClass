37da742c06ac4daf0cd40129054fce19b8ea4e2e
shimarrudz <vic.shima.vss@gmail.com>
2023-08-16 20:34:03 -0300

    feat: create kotlin path

---
+++ ApresenteSe/ApresenteSe.kt
@@ -0,0 +1,9 @@
+fun main(){
+    
+    //Declarando variáveis
+    var nome = "SQUAD" //string
+    var capacidade = 5.1f //float
+    var inicial = "V" //string
+
+    println("A squad chama $nome, capacidade $capacidade minha inicial $inicial")
+}
