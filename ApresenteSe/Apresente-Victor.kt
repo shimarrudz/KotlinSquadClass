3f5eda90b37e7bca0ece042b97fde4f045ee7155
shimarrudz <vic.shima.vss@gmail.com>
2023-08-16 20:53:17 -0300

    feat: create branch and develop

---
+++ ApresenteSe/Apresente-Victor.kt
@@ -0,0 +1,9 @@
+fun main(){
+    
+    //Declarando variáveis
+    var nome = "Atrasados" //string
+    var capacidade = 8.1f //float
+    var inicial = "A" //string
+
+    println("A squad chama $nome, capacidade $capacidade minha inicial $inicial")
+}
